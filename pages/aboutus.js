01e1726ad3bf3d40553b003359d2a893d66164b0
vineeth-10-10 <vineethkalewar62@gmail.com>
2025-05-07 23:02:39 +0530

    Initial nextjs and made contactus component

---
+++ pages/aboutus.js
@@ -0,0 +1,5 @@
+function AboutUs() {
+  return <h1>This is aabpout us page path is domain-name/aboutus</h1>;
+}
+
+export default AboutUs;
---
+++ pages/news.js
@@ -0,0 +1,6 @@
+
+
+function NewsPage (){
+    return <h1>This is News Page..</h1>
+}
+export default NewsPage;
