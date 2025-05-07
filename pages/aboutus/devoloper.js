81a1ee64f614802dc7d92f120042e6de73e133c9
vineeth-10-10 <vineethkalewar62@gmail.com>
2025-05-07 23:16:12 +0530

    Using nested routing using folders i have a path for /aboutus/devoloper and /news/something

---
+++ pages/aboutus/devoloper.js
@@ -0,0 +1,7 @@
+
+
+function Devoloper (){
+    return <h1>My name is Vineeth i am the devoloper</h1>
+}
+
+export default Devoloper;
---
+++ pages/news/something/index.js
@@ -0,0 +1,7 @@
+
+
+function Something (){
+    return <h1>this is inside news as a nested route ..../news/something</h1>
+}
+
+export default Something;
