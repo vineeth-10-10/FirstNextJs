69b7b04b4e61c48ad9971b44b7b8bd5dbc8760ce
vineeth-10-10 <vineethkalewar62@gmail.com>
2025-05-08 12:32:55 +0530

    Created a details object to check my dynamic route adn find details of developer

--- pages/aboutus/devoloper.js
+++
@@ -1,7 +0,0 @@
-
-
-function Devoloper (){
-    return <h1>My name is Vineeth i am the devoloper</h1>
-}
-
-export default Devoloper;
--- pages/news/something/index.js
+++
@@ -1,7 +0,0 @@
-
-
-function Something (){
-    return <h1>this is inside news as a nested route ..../news/something</h1>
-}
-
-export default Something;
