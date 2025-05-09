efb44afb54f312135d668caea81786205f0828f7
vineeth-10-10 <vineethkalewar62@gmail.com>
2025-05-09 14:01:56 +0530

    Learnt why we use getStaticProps and use getStaticPaths for dynamic prerendering using fallback inside paths

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -5,9 +5,51 @@ export default function DetailsPage() {
   return (
     <MeetUpDetails
       image="https://imgs.search.brave.com/sPw7U2zcMOOZUlaHMefinvBFsBatF1ochTUouTCCxpA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjEy/NjMwNDc4L3Bob3Rv/L2hvbmcta29uZy1u/aWdodC1zY2VuZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/TGFxWlc2YU5WRFpt/bDgyc29nRFlfSklz/WmZsY0tvWklYTzh5/ZWljczVCQT0"
-      title="SOme title"
+      title=" title"
       address="Somewhwer in hong kong"
       description="1st meetup like this"
     />
   );
+}
+
+
+export async function getStaticPaths(){
+    return{
+        fallback:true,
+        paths:[
+            {
+                params:{
+                    meetupId:"m1"
+                }
+            },
+            {
+                params:{
+                    meetupId:"m2"
+                }
+            },
+            {
+                params:{
+                    meetupId:"m3"
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context){
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+
+    return{
+        props:{
+            meetupData:{
+                image:"https://imgs.search.brave.com/sPw7U2zcMOOZUlaHMefinvBFsBatF1ochTUouTCCxpA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjEy/NjMwNDc4L3Bob3Rv/L2hvbmcta29uZy1u/aWdodC1zY2VuZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/TGFxWlc2YU5WRFpt/bDgyc29nRFlfSklz/WmZsY0tvWklYTzh5/ZWljczVCQT0",
+                id:meetupId,
+                title:"Some title",
+                address:"Somewhwer in hong kong",
+                description:"1st meetup like this"
+            }
+        }
+    }
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import MeetupList from "@/components/meetups/MeetupList";
 
+
 const DUMMY = [
   {
-    id: 1,
+    id: 'm1',
     title: "My 1st meetup",
     image:
       "https://imgs.search.brave.com/sPw7U2zcMOOZUlaHMefinvBFsBatF1ochTUouTCCxpA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjEy/NjMwNDc4L3Bob3Rv/L2hvbmcta29uZy1u/aWdodC1zY2VuZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/TGFxWlc2YU5WRFpt/bDgyc29nRFlfSklz/WmZsY0tvWklYTzh5/ZWljczVCQT0",
     address: "HongKong",
   },
   {
-    id: 2,
+    id: 'm2',
     title: "2nd meet up",
     image:
       "https://imgs.search.brave.com/sPw7U2zcMOOZUlaHMefinvBFsBatF1ochTUouTCCxpA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjEy/NjMwNDc4L3Bob3Rv/L2hvbmcta29uZy1u/aWdodC1zY2VuZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/TGFxWlc2YU5WRFpt/bDgyc29nRFlfSklz/WmZsY0tvWklYTzh5/ZWljczVCQT0",
@@ -17,6 +18,19 @@ const DUMMY = [
   },
 ];
 
-export default function HomePage() {
-  return <MeetupList meetups={DUMMY} />;
+export default function HomePage(props) {
+    
+
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps () {
+    //making api calls
+
+    return{
+        props:{
+            meetups:DUMMY,
+            revalidate: 100,
+        },
+    };
 }
