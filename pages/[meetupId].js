537f84edcf73c17c45f4d2af4529144bd46095ad
vineeth-10-10 <vineethkalewar62@gmail.com>
2025-05-09 22:53:15 +0530

    I am using Mongodb similiar code used in index.js (default index) receiving the data and checking if id matches and then sending the props to MeetUpDetails component its working i can see details page upon click

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,13 +1,15 @@
+import {MongoClient} from 'mongodb';
+
 import MeetUpDetails from "@/components/meetups/MeetUpDetails";
 
 
-export default function DetailsPage() {
+export default function DetailsPage(props) {
   return (
     <MeetUpDetails
-      image="https://imgs.search.brave.com/sPw7U2zcMOOZUlaHMefinvBFsBatF1ochTUouTCCxpA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjEy/NjMwNDc4L3Bob3Rv/L2hvbmcta29uZy1u/aWdodC1zY2VuZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/TGFxWlc2YU5WRFpt/bDgyc29nRFlfSklz/WmZsY0tvWklYTzh5/ZWljczVCQT0"
-      title=" title"
-      address="Somewhwer in hong kong"
-      description="1st meetup like this"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
@@ -38,16 +40,30 @@ export async function getStaticPaths(){
 
 export async function getStaticProps(context){
     const meetupId = context.params.meetupId;
-    console.log(meetupId);
+    // console.log(meetupId);
+
+    const client = await MongoClient.connect(
+        "mongodb+srv://admin:oYkZrwsZqlZWUoBu@cluster0.ajzk4bz.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+      );
+      const db = client.db();
+      const meetupCollection = db.collection('meetups');
+
+      const meetups = await meetupCollection.find().toArray();
+
+      const currentMeetup = meetups.find((meetup)=>meetup._id.toString() === meetupId);
+
+      console.log(currentMeetup);
+
+      client.close();
 
     return{
         props:{
             meetupData:{
-                image:"https://imgs.search.brave.com/sPw7U2zcMOOZUlaHMefinvBFsBatF1ochTUouTCCxpA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjEy/NjMwNDc4L3Bob3Rv/L2hvbmcta29uZy1u/aWdodC1zY2VuZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/TGFxWlc2YU5WRFpt/bDgyc29nRFlfSklz/WmZsY0tvWklYTzh5/ZWljczVCQT0",
+                image:currentMeetup.image,
                 id:meetupId,
-                title:"Some title",
-                address:"Somewhwer in hong kong",
-                description:"1st meetup like this"
+                title:currentMeetup.title,
+                address:currentMeetup.address,
+                description:currentMeetup.description,
             }
         }
     }
